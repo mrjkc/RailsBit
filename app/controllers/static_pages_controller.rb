d178fd74f3f490646e10a88f023b4f0171fa01bd
mrjkc <juliuskvedaras@yahoo.ie>
2017-04-19 17:19:05 +0000

    too amny to list

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,12 @@ class StaticPagesController < ApplicationController
     def home
         @articles = Article.all
         @videos = Video.all
+        @quotes = Qoute.all
     end
     
     def backend
         @articles = Article.all
         @videos = Video.all
     end
+    
 end
