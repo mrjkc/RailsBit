465ee8b95ad84c396192abfa45aca731eee29152
mrjkc <juliuskvedaras@yahoo.ie>
2017-04-25 16:51:30 +0000

    fixed quote method

--- app/models/qoute.rb
+++ app/models/qoute.rb
@@ -1,14 +1,13 @@
 class Qoute < ActiveRecord::Base
             
-    @current_date_array = ((Time.now).to_s.split("-"))[2].split(" ")
-    @counter = 1
-    
     def self.process_quote(q)
+        current_date_array = ((Time.now).to_s.split("-"))[2].split(" ")
+        counter = 1
         q.each do |x|
-            if @counter.to_s == @current_date_array[0]
+            if counter.to_s == current_date_array[0]
                 return x
             end 
-            @counter += 1
+            counter += 1
         end
     end
 end
