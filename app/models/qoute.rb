edd2ce6cd6b2b88ffc568bbb12613747c3cd68f7
mrjkc <juliuskvedaras@yahoo.ie>
2017-05-01 11:01:38 +0000

    fixed quotes when date is 01...

--- app/models/qoute.rb
+++ app/models/qoute.rb
@@ -4,6 +4,9 @@ class Qoute < ActiveRecord::Base
         current_date_array = ((Time.now).to_s.split("-"))[2].split(" ")
         counter = 1
         q.each do |x|
+            if current_date_array[0][0,1] == '0'
+                current_date_array[0] = current_date_array[0][1,1]
+            end
             if counter.to_s == current_date_array[0]
                 return x
             end 
